9a27571a615396bac6b3a9cbbea7ccb1f6abd4dd
unknown <randeprajbnanshi0011@gmail.com>
2021-08-03 12:36:04 +0545

    protfolio

---
+++ script.js
@@ -0,0 +1,64 @@
+var line = document.querySelector('.lines');
+var list = document.getElementById('list');
+
+//var tran = document.getElementById('tran');
+//function myFun(){
+//list.classList.toggle("active");
+//}
+
+
+line.onclick = () => {
+  list.classList.toggle("active");
+
+}
+
+function tra() {
+  list.classList.remove("active");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// var names = ['coding', 'website design', 'php', 'wordpress theme', 'logo'];
+
+
+// names.forEach(amu);
+
+// function amu(heding, index){
+// 	document.getElementById("demo").append( `<h1>${heding}</h1>`)//innerHTML = '<h1>'+heding+'</h1>';
+// }
+
+
+
+// var html = `      <div class="webCard">
+// <div class="imgDiv">
+// <img src="bg.jpg" alt="">
+// </div>
+// <div class="cardContain">
+// <h1>${coding}</h1>
+// <p>hello i am a website designer.i.i am aprofessional designer .i have many design whi you will love it</p>
+// <a href="#">view</a>
+// </div>
+// </div>`;
+
+
+// document.getElementById("demo").innerHTML = html.repeat(3);
+
+
+// var names = ['ranodep', 'simal', 'kishor'];
+
+// names.forEach(fun);
+// function fun(a, b){
+// 	console.log(`i am ${a}`);
+// }
+
+
+
